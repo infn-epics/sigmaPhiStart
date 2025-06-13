8a98792831e726fcbe815d79c356cb7afddd3d9b
amichelo <andrea.michelotti@lnf.infn.it>
2025-06-13 13:15:34 +0300

    unimag states

--- iocBoot/ioctest/st.cmd
+++ iocBoot/ioctest/st.cmd
@@ -6,10 +6,10 @@
 dbLoadDatabase "../../dbd/sigmaPhiStart.dbd"
 sigmaPhiStart_registerRecordDeviceDriver(pdbbase)
 
-epicsEnvSet("PREFIX", "LEL:SPSU02")
+epicsEnvSet("PREFIX", "LEL:MAG:SPSU01:SOL01:COIL01")
 
 
-drvAsynIPPortConfigure("TEST", "10.16.4.40:502", 0, 0, 0)
+drvAsynIPPortConfigure("TEST", "10.16.4.45:502", 0, 0, 0)
 modbusInterposeConfig ("TEST", 0, 2000, 0)
 drvModbusAsynConfigure ("TEST_RD",  "TEST", 1,  3, 1, 13, 0, 1000, "SigmaPhi")
 drvModbusAsynConfigure ("TEST_CMD",  "TEST", 1,  6, 0,  1, 0, 1000, "SigmaPhi")
@@ -19,8 +19,8 @@ drvModbusAsynConfigure ("TEST_SET",  "TEST", 1, 16, 5,  2, 0, 1000, "SigmaPhi")
 
 # Load section for driver 'Agilent-4UHV'
 
-dbLoadRecords("$(TOP)/db/sigmaPhiStart.template","device=$(PREFIX):SOL01:COIL02, rd_port=TEST_RD, cmd_port=TEST_CMD,set_port=TEST_SET,max=1")
-dbLoadRecords("$(TOP)/db/unimag.db","device=$(PREFIX):SOL01:COIL02")
+dbLoadRecords("$(TOP)/db/sigmaPhiStart.template","device=$(PREFIX), rd_port=TEST_RD, cmd_port=TEST_CMD,set_port=TEST_SET,max=1")
+dbLoadRecords("$(TOP)/db/unimag.db","device=$(PREFIX)")
 
 
 #
